b7ec7deaa2c4d4609e6430c4d596e12394de486f
bonesbroken <145737816+bonesbroken@users.noreply.github.com>
2025-12-24 12:13:16 -0500

    1

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  base: '/',
+  base: '/savingkidneys.github.io/',
   plugins: [vue()],
   resolve: {
     alias: {
